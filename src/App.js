d7cf755a85203291114d5fee384a06c6467c910e
Kacimi Jugurtha <kjugurtha1711@gmail.com>
2020-08-07 14:23:45 +0100

    Adding login button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import TodoContainer from './components/TodoContainer';
 
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
+import Button from '@material-ui/core/Button';
 
 const theme = createMuiTheme({
   palette: {
@@ -18,7 +19,12 @@ function App() {
   return (
     <div className='App'>
       <ThemeProvider theme={theme}>
-        <Container maxWidth='sm'>
+        <Container maxWidth='md'>
+          <div className='login-container'>
+            <Button variant='outlined' color='primary' className='login-btn'>
+              Login using Google
+            </Button>
+          </div>
           <h1>Todo App</h1>
           <AddTodo />
           <TodoContainer />
