3823da805e03818c67f45026bfb8c25213815b9f
Kacimi Jugurtha <kjugurtha1711@gmail.com>
2020-08-10 23:28:31 +0100

    Reducers Configured

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -4,7 +4,19 @@ const authReducer = (state = initState, action) => {
   switch (action.type) {
     case 'LOGIN_SUCCESS':
       console.log(action.type);
-      break;
+      return state;
+
+    case 'LOGIN_ERROR':
+      console.log(action.type, action.err);
+      return state;
+
+    case 'LOGOUT_SUCCESS':
+      console.log(action.type);
+      return state;
+
+    case 'LOGOUT_ERROR':
+      console.log(action.type, action.err);
+      return state;
 
     default:
       return state;
